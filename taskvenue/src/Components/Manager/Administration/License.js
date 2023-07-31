1fca13d8aa5313830dee28df89d8661a658900e7
Lukaszgor <lukasz.gg13@gmail.com>
2023-08-01 00:52:05 +0200

    licence view refactoring

--- taskvenue/src/Components/Manager/Administration/License.js
+++ taskvenue/src/Components/Manager/Administration/License.js
@@ -1,51 +1,68 @@
 import React, { useState, useEffect } from 'react';
 import { TextField, Button, Grid, Container, Typography, Select, MenuItem } from '@mui/material';
 import supabase from '../../../supabaseClient';
-import FetchSupabaseData from '../../../Config/FetchSupabaseData';
 import { useTranslation } from 'react-i18next';
 
 const License = () => {
   const { t, i18n } = useTranslation();
   const [validity_date, setValidity_date] = useState('');
-  const [receivedData, setReceivedData] = useState({ userId: '', idConfiguration: '', profileType: '' });
   const [isLoading, setIsLoading] = useState(true);
   const [hasError, setHasError] = useState(false);
+const [userID, setUserID] = useState('');
+const [idConfig, setIdConfiguration] = useState('');
 
-  const handleGetData = (userId, idConfiguration, profileType) => {
-    setReceivedData({ userId, idConfiguration, profileType });
-  };
+useEffect(() => {
+        if (idConfig) {
+          fetchValidityDate();
+        }
+      }, [idConfig]);
 
-  const fetchValidityDate = async () => {
-    try {
-      const { data, error } = await supabase
-        .from('configurations')
-        .select('validity_date')
-        .eq('id', receivedData.idConfiguration)
-        .single();
-
-      if (error) {
-        setHasError(true);
-      } else if (data) {
-        setValidity_date(data.validity_date);
-      }
-
-      setIsLoading(false);
-    } catch (error) {
-      console.error('error:', error);
-      setHasError(true);
-      setIsLoading(false);
-    }
-  };
+const fetchValidityDate = async () => {
+        try {
+          const { data, error } = await supabase
+            .from('configurations')
+            .select('validity_date')
+            .eq('id', idConfig)
+            .single();
+    
+          if (error) {
+            setHasError(true);
+          } else if (data) {
+            setValidity_date(data.validity_date);
+          }
+    
+          setIsLoading(false);
+        } catch (error) {
+          console.error('error:', error);
+          setHasError(true);
+          setIsLoading(false);
+        }
+      };
 
   useEffect(() => {
-    if (receivedData.idConfiguration) {
-      fetchValidityDate();
+    const checkSession = async () => {
+        const { data } = await supabase.auth.getSession();
+        if (data.session) {
+            setUserID(data.session.user.id)
+            fetchData(data.session.user.id)
+        }
+      };
+      checkSession();
+  });
+  const fetchData = async (userId) => {
+    const { data: profileData, error: profileError } = await supabase
+        .from('profiles')
+        .select()
+        .eq('id', userId)
+        .single();
+    if (profileError) {
+        console.log(profileError);
+    } else if (profileData) {
+        setIdConfiguration(profileData.id_configuration);
     }
-  }, [receivedData.idConfiguration]);
-
+}
   return (
     <div>
-      <FetchSupabaseData sendData={handleGetData}></FetchSupabaseData>
       {isLoading ? (
         <p>{t("Landing...")}</p>
       ) : hasError ? (
