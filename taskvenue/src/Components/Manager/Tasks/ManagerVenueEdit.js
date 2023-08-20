613229f4c37c1f6c5e552968baa1e35e2d3a071e
Lukaszgor <lukasz.gg13@gmail.com>
2023-08-20 10:40:51 +0200

    added venue component

--- taskvenue/src/Components/Manager/Tasks/ManagerVenueEdit.js
+++ taskvenue/src/Components/Manager/Tasks/ManagerVenueEdit.js
@@ -1,12 +1,193 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import {Grid,Container,Select,MenuItem,FormControl,InputLabel,Typography,} from '@mui/material';
+import { useParams } from 'react-router-dom';
+import supabase from '../../../supabaseClient';
+import { useTranslation } from 'react-i18next';
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
 
 const ManagerVenueEdit = () => {
-    return (
-        <div>
-<p>ManagerVenueEdit</p>
-      </div>
-      );
+    const [open, setOpen] = useState(false);
+  const { t, i18n } = useTranslation();
+  const { id } = useParams();
+  const [idConfig, setIdConfiguration] = useState('');
+  const [selectedVenueId, setSelectedVenueId] = useState('');
+  const [userID, setUserID] = useState('');
+  const [venues, setVenues] = useState([]);
+  const [selectedVenue, setSelectedVenue] = useState(null);
+
+  useEffect(() => {
+    const checkSession = async () => {
+      const { data } = await supabase.auth.getSession();
+      if (data.session) {
+        setUserID(data.session.user.id);
+        fetchData(data.session.user.id);
+      }
+    };
+    checkSession();
+  }, []);
+
+  const fetchData = async (userId) => {
+    const { data: profileData, error: profileError } = await supabase
+      .from('profiles')
+      .select('id_configuration')
+      .eq('id', userId)
+      .single();
+
+    if (profileError) {
+      console.log(profileError);
+    } else if (profileData) {
+      setIdConfiguration(profileData.id_configuration);
+    }
+  };
+
+  const handleFetchVenues = async (idConfig) => {
+    const { data, error } = await supabase
+      .from('venues')
+      .select()
+      .eq('id_configuration', idConfig);
+
+    if (error) {
+      console.log(error);
+    }
+    if (data) {
+      setVenues(data);
+    }
+  };
+
+  const handleFetchVenueID = async (idConfig) => {
+    const { data, error } = await supabase
+      .from('tasks')
+      .select('id_venue')
+      .eq('id_configuration', idConfig);
+
+    if (error) {
+      // Handle error if needed
+    }
+    if (data && data.id_venue !== undefined) {
+      setSelectedVenueId(data.id_venue);
+    }
+  };
+
+  const handleFetchVenueInfo = async (venueId) => {
+    const { data, error } = await supabase
+      .from('venues')
+      .select()
+      .eq('id', venueId)
+      .single();
+
+    if (error) {
+      // Handle error if needed
+    }
+    if (data) {
+      setSelectedVenue(data);
+    }
+  };
+  const handleFetchTask = async () => {
+    const { data, error } = await supabase
+      .from('tasks')
+      .select('id_venue')
+      .eq('id', id)
+      .single();
+    if (error) {
+      // Handle error if needed
+    }
+    if (data) {
+        setSelectedVenueId(data.id_venue);
+    }
+  };
+
+  useEffect(() => {
+    if (idConfig) {
+      handleFetchVenueID(idConfig);
+      handleFetchVenues(idConfig);
+      handleFetchTask();
+    }
+  }, [idConfig]);
+
+  useEffect(() => {
+    if (selectedVenueId) {
+      handleFetchVenueInfo(selectedVenueId);
+    }
+  }, [selectedVenueId]);
+
+  const handleChangeVenue = (event) => {
+    const value = event.target.value;
+    setSelectedVenueId(value);
+    handleUpdateTask(value);
+  };
+
+  const handleUpdateTask = async (selectedVenueId) => {
+    const { data, error } = await supabase
+      .from('tasks')
+      .update([
+        {
+            id_venue: selectedVenueId,
+        },
+      ])
+      .eq('id', id);
+    handleClickAlert();
+    if (error) {
+      console.log(error);
+    }
+    if (data) {
+ 
+    }
+  };
+
+//alert
+  const handleClickAlert = () => {
+    setOpen(true);
+  };
+
+  const handleCloseAlert = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Container maxWidth="md">
+        <Grid item xs={12} sm={6}>
+          <FormControl fullWidth>
+            <InputLabel id="venue-select-select-label">
+              {t('Select Venue')}
+            </InputLabel>
+            <Select
+              labelId="venue-select-label"
+              id="venue-select"
+              value={selectedVenueId}
+              onChange={handleChangeVenue}
+              label={t('Select Venue')}
+            >
+              {venues.map((venue) => (
+                <MenuItem key={venue.id} value={venue.id}>
+                  {venue.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+        {selectedVenue && (
+          <Grid item xs={12}>
+            <Typography variant="h10">{t('Description')}</Typography>
+            <Typography variant="h6">{selectedVenue.description}</Typography>
+            <Typography variant="h10">{t('Address')}</Typography>
+            <Typography variant="h6">{selectedVenue.GPS_location}</Typography>
+          </Grid>
+        )}
+                <Snackbar
+          open={open}
+          autoHideDuration={2000}
+          onClose={handleCloseAlert}
+        >
+          <Alert severity="success">{t('Updated')}!</Alert>
+        </Snackbar>
+      </Container>
+    </div>
+  );
 };
 
 export default ManagerVenueEdit;
