c2d621984ec8881ffeb4c47082b953c6f7476c23
Lukaszgor <lukasz.gg13@gmail.com>
2023-08-27 10:41:49 +0200

    new type of filtres

--- taskvenue/src/Pages/Manager/VenueDashboard.js
+++ taskvenue/src/Pages/Manager/VenueDashboard.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Card, CardContent, Typography, Grid, TextField } from '@mui/material';
+import { Card, CardContent, Typography, Grid, TextField, Button, Dialog, DialogContent, DialogTitle } from '@mui/material';
 import supabase from '../../supabaseClient';
-import Button from '@mui/material/Button';
 import { useNavigate } from "react-router-dom"
 import ManagerNavBar from '../../Components/NavigationBar/ManagerNavBar';
 import { useTranslation } from "react-i18next";
+import FilterListIcon from '@mui/icons-material/FilterList'; 
 
 const VenueDashboard = () => {
     const [venues, setVenues] = useState([]);
@@ -17,146 +17,185 @@ const VenueDashboard = () => {
 
     const [userID, setUserID] = useState('');
     const [idConfig, setIdConfiguration] = useState('');
+    const [isFilterPopupOpen, setIsFilterPopupOpen] = useState(false);
     
-        useEffect(() => {
-            const checkSession = async () => {
-              const { data } = await supabase.auth.getSession();
-              if (data.session) {
+    useEffect(() => {
+        const checkSession = async () => {
+            const { data } = await supabase.auth.getSession();
+            if (data.session) {
                 setUserID(data.session.user.id);
                 fetchData(data.session.user.id);
-              }
-            };
-            checkSession();
-          }, []);
-          
-          const fetchData = async (userId) => {
-            const { data: profileData, error: profileError } = await supabase
-                .from('profiles')
-                .select('id_configuration')
-                .eq('id', userId)
-                .single();
-            if (profileError) {
-                console.log(profileError);
-            } else if (profileData) {
-                setIdConfiguration(profileData.id_configuration);
             }
+        };
+        checkSession();
+    }, []);
+
+    const fetchData = async (userId) => {
+        const { data: profileData, error: profileError } = await supabase
+            .from('profiles')
+            .select('id_configuration')
+            .eq('id', userId)
+            .single();
+        if (profileError) {
+            console.log(profileError);
+        } else if (profileData) {
+            setIdConfiguration(profileData.id_configuration);
         }
-     useEffect(() => {
-            if (idConfig) {
-                fetchVenues(idConfig);
-            }
-          }, [idConfig]);
-
-// Filtruj dane na podstawie nazwy i statusu
-useEffect(() => {
-    let filteredData = venues;
-
-    if (searchName !== '') {
-      filteredData = filteredData.filter((venue) => venue.name.toLowerCase().includes(searchName.toLowerCase()));
     }
-    if (searchNumber !== '') {
-        filteredData = filteredData.filter((venue) => venue.id.toString().includes(searchNumber));
-      }
-      if (searchContractor !== '') {
-        filteredData = filteredData.filter((venue) => venue.contractor?.nameOrCompanyName.toLowerCase().includes(searchContractor.toLowerCase()));
-      }
 
-      setFilteredVenues(filteredData);
-  }, [venues, searchName,searchNumber,searchContractor]);
-  
-        const fetchVenues = async (idConfiguration) => {
-            const { data, error } = await supabase
-                .from('venues')
-                .select(`*,
+    useEffect(() => {
+        if (idConfig) {
+            fetchVenues(idConfig);
+        }
+    }, [idConfig]);
+
+    useEffect(() => {
+        let filteredData = venues;
+
+        if (searchName !== '') {
+            filteredData = filteredData.filter((venue) => venue.name.toLowerCase().includes(searchName.toLowerCase()));
+        }
+        if (searchNumber !== '') {
+            filteredData = filteredData.filter((venue) => venue.id.toString().includes(searchNumber));
+        }
+        if (searchContractor !== '') {
+            filteredData = filteredData.filter((venue) => venue.contractor?.nameOrCompanyName.toLowerCase().includes(searchContractor.toLowerCase()));
+        }
+
+        setFilteredVenues(filteredData);
+    }, [venues, searchName, searchNumber, searchContractor]);
+
+    const fetchVenues = async (idConfiguration) => {
+        const { data, error } = await supabase
+            .from('venues')
+            .select(`*,
                 contractor (
                     nameOrCompanyName
                 )
-                ` ) .eq('id_configuration', idConfig);
+            ` )
+            .eq('id_configuration', idConfig);
 
         if (error) {
-          console.error(error);
+            console.error(error);
         } else {
-          setVenues(data);
+            setVenues(data);
         }
-      };
+    };
 
-      //redirection to details venue
-      const handleButtonClickVenueDetails=(venue)=>{
-        navigate('/VenueDetalils/'+venue.id)
-    }
-    const addNewVenue=()=>{
+    const handleButtonClickVenueDetails = (venue) => {
+        navigate('/VenueDetalils/' + venue.id)
+    };
+
+    const addNewVenue = () => {
         navigate('/AddNewVenue')
-    }
+    };
+
+    const applyFilters = () => {
+        let filteredData = venues;
+
+        if (searchName !== '') {
+            filteredData = filteredData.filter((venue) => venue.name.toLowerCase().includes(searchName.toLowerCase()));
+        }
+        if (searchNumber !== '') {
+            filteredData = filteredData.filter((venue) => venue.id.toString().includes(searchNumber));
+        }
+        if (searchContractor !== '') {
+            filteredData = filteredData.filter((venue) => venue.contractor?.nameOrCompanyName.toLowerCase().includes(searchContractor.toLowerCase()));
+        }
+
+        setFilteredVenues(filteredData);
+        setIsFilterPopupOpen(false);
+    };
 
     return (
         <div>
             <ManagerNavBar></ManagerNavBar>
-<p></p>
- <p></p>
-            <div style={{ marginBottom: '20px' }}>
+            <p></p>
             <Button  style={{ marginLeft: '20px',marginBottom: '20px' }} type="submit" variant="contained" color="primary"  onClick={addNewVenue} >
                 {t("Add")}
               </Button>
-          <TextField
-          style={{ marginLeft: '20px',marginBottom: '20px' }}
-            label={t("Search by number")}
-            variant="outlined"
-            value={searchNumber}
-            onChange={(e) => setsearchNumber(e.target.value)}
-          />
-           <TextField
-          style={{ marginLeft: '20px',marginBottom: '20px' }}
-            label={t("Search by name")}
-            variant="outlined"
-            value={searchName}
-            onChange={(e) => setSearchName(e.target.value)}
-          />
-          <TextField
-          style={{ marginLeft: '20px',marginBottom: '20px' }}
-            label={t("Search by contractor")}
-            variant="outlined"
-            value={searchContractor}
-            onChange={(e) => setsearchContractor(e.target.value)}
-          />
-     
+            <Button
+                style={{ marginLeft: '20px', marginBottom: '20px' }}
+                variant="contained"
+                color="primary"
+                onClick={() => setIsFilterPopupOpen(true)}
+                startIcon={<FilterListIcon />}
+            >
+                {t("Open Filter")}
+            </Button>
+            <Dialog open={isFilterPopupOpen} onClose={() => setIsFilterPopupOpen(false)}>
+    <DialogTitle>{t("Filter Venues")}</DialogTitle>
+    <DialogContent>
+        <div style={{ marginBottom: '16px' }}>
+            <TextField
+                label={t("Search by number")}
+                variant="outlined"
+                value={searchNumber}
+                onChange={(e) => setsearchNumber(e.target.value)}
+                style={{ marginBottom: '8px' }} 
+            />
         </div>
-        {venues &&(
-        <Grid container spacing={3}>
-          {filteredVenues.map((venue) => (
-            <Grid key={venue.id} item xs={12} sm={6} md={4} lg={3}>
-              <Card>
-                <CardContent>
-                  <Typography variant="h6" gutterBottom>
-                    ID: {venue.id}
-                  </Typography>
-                  <Typography variant="h6" gutterBottom>
-                  {t("Name")} : {venue.name}
-                  </Typography>
-                  <Typography variant="body2" color="textSecondary">
-                  {t("Description")} : {venue.description}
-                  </Typography>
-                  <Typography variant="body2" color="textSecondary">
-                  {t("Contractor")} : {venue.contractor?.nameOrCompanyName}
-                  </Typography>
-                  <p></p>
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={() => handleButtonClickVenueDetails(venue)}
-                  >
-                    {t("details")}
-                  </Button>
-                </CardContent>
-              </Card>
+        <div style={{ marginBottom: '16px' }}>
+            <TextField
+                label={t("Search by name")}
+                variant="outlined"
+                value={searchName}
+                onChange={(e) => setSearchName(e.target.value)}
+                style={{ marginBottom: '8px' }} 
+            />
+        </div>
+        <div style={{ marginBottom: '16px' }}>
+            <TextField
+                label={t("Search by contractor")}
+                variant="outlined"
+                value={searchContractor}
+                onChange={(e) => setsearchContractor(e.target.value)}
+                style={{ marginBottom: '8px' }} 
+            />
+        </div>
+        <Button
+            variant="contained"
+            color="primary"
+            onClick={applyFilters}
+            style={{ marginTop: '16px' }} 
+        >
+            {t("Apply Filters")}
+        </Button>
+    </DialogContent>
+</Dialog>
+
+            <Grid container spacing={3}>
+                {filteredVenues.map((venue) => (
+                    <Grid key={venue.id} item xs={12} sm={6} md={4} lg={3}>
+                        <Card>
+                            <CardContent>
+                                <Typography variant="h6" gutterBottom>
+                                    ID: {venue.id}
+                                </Typography>
+                                <Typography variant="h6" gutterBottom>
+                                    {t("Name")} : {venue.name}
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary">
+                                    {t("Description")} : {venue.description}
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary">
+                                    {t("Contractor")} : {venue.contractor?.nameOrCompanyName}
+                                </Typography>
+                                <p></p>
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    onClick={() => handleButtonClickVenueDetails(venue)}
+                                >
+                                    {t("details")}
+                                </Button>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                ))}
             </Grid>
-          ))}
-        </Grid>
-        )}
-      </div>      
+        </div>
     );
-
-
-
 };
 
 export default VenueDashboard;
