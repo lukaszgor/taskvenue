15f555e16dee7cdff453aff42f25f46603b9993d
Lukaszgor <lukasz.gg13@gmail.com>
2023-08-30 15:29:20 +0200

    improved license view

--- taskvenue/src/Components/Manager/Administration/License.js
+++ taskvenue/src/Components/Manager/Administration/License.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { TextField, Button, Grid, Container, Typography, Select, MenuItem } from '@mui/material';
+import { TextField, Button, Grid, Container, Typography, Select, MenuItem,Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
 import supabase from '../../../supabaseClient';
 import { useTranslation } from 'react-i18next';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const License = () => {
   const { t, i18n } = useTranslation();
@@ -10,6 +11,7 @@ const License = () => {
   const [hasError, setHasError] = useState(false);
 const [userID, setUserID] = useState('');
 const [idConfig, setIdConfiguration] = useState('');
+const [name, setName] = useState('');
 
 useEffect(() => {
         if (idConfig) {
@@ -21,7 +23,7 @@ const fetchValidityDate = async () => {
         try {
           const { data, error } = await supabase
             .from('configurations')
-            .select('validity_date')
+            .select('validity_date,name')
             .eq('id', idConfig)
             .single();
     
@@ -29,6 +31,8 @@ const fetchValidityDate = async () => {
             setHasError(true);
           } else if (data) {
             setValidity_date(data.validity_date);
+            setName(data.name);
+
           }
     
           setIsLoading(false);
@@ -63,13 +67,31 @@ const fetchValidityDate = async () => {
 }
   return (
     <div>
-      {isLoading ? (
+    <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography variant="h6" fontWeight="bold">{t('License')}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography></Typography>
+          {isLoading ? (
         <p>{t("Landing...")}</p>
       ) : hasError ? (
         <p>{t("An error occurred while downloading data.")}</p>
       ) : (
-        <p>{t("Your license is valid until:")} {validity_date}</p>
+        <Typography>{t("Your license is valid until:")} {validity_date}</Typography>
       )}
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography variant="h6" fontWeight="bold">{t('About configuration')}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>{name}</Typography>
+        </AccordionDetails>
+      </Accordion>
+
     </div>
   );
 };
