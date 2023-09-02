7d47d67468b947c99ed65cdce9465b1083c44839
Lukaszgor <lukasz.gg13@gmail.com>
2023-09-03 00:52:41 +0200

    added arrows in administration and tasks details

--- taskvenue/src/Pages/Manager/Administration.js
+++ taskvenue/src/Pages/Manager/Administration.js
@@ -3,7 +3,7 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import ManagerNavBar from '../../Components/NavigationBar/ManagerNavBar';
 import { useTranslation } from "react-i18next";
 import License from '../../Components/Manager/Administration/License';
@@ -15,80 +15,131 @@ import TaskTypeDictionary from '../../Components/Manager/Administration/TaskType
 import ManagerAdministrationBreadcrumbs from '../../Components/Breadcrumbs/mainBreadcrumbs/ManagerAdministrationBreadcrumbs';
 
 function TabPanel(props) {
-    const { children, value, index, ...other } = props;
-  
-    return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box sx={{ p: 3 }}>
-            <Typography component={'span'} variant={'body2'}>{children}</Typography>
-          </Box>
-        )}
-      </div>
-    );
-  }
-  
-  TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.number.isRequired,
-    value: PropTypes.number.isRequired,
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography component={'span'} variant={'body2'}>
+            {children}
+          </Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
   };
-  
-  function a11yProps(index) {
-    return {
-      id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`,
-    };
-  }
+}
 
 function Administration() {
-    const { t, i18n } = useTranslation();
-    const [value, setValue] = useState(0);
-    const handleChange = (event, newValue) => {
-      setValue(newValue);
-    };
-    return (
-      <div>
-             <ManagerNavBar></ManagerNavBar>
-             <ManagerAdministrationBreadcrumbs></ManagerAdministrationBreadcrumbs>
-            <Box sx={{ width: '100%' }}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-          <Tab label={t("Users")} {...a11yProps(0)} />
-          <Tab label={t("License")} {...a11yProps(1)} />
-          <Tab label={t("Settings")}  {...a11yProps(2)} />
-          <Tab label={t("Services")}  {...a11yProps(3)} />
-          <Tab label={t("Contractors")}  {...a11yProps(4)} />
-          <Tab label={t("Task type")}  {...a11yProps(5)} />
-        </Tabs>
+  const { t, i18n } = useTranslation();
+  const [value, setValue] = useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleScrollRight = () => {
+    if (value < 5) {
+      setValue(value + 1);
+    }
+  };
+
+  const handleScrollLeft = () => {
+    if (value > 0) {
+      setValue(value - 1);
+    }
+  };
+
+  return (
+    <div>
+      <ManagerNavBar></ManagerNavBar>
+      <ManagerAdministrationBreadcrumbs></ManagerAdministrationBreadcrumbs>
+      <Box sx={{ width: '100%' }}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider', position: 'relative' }}>
+          {value > 0 && (
+            <button
+              onClick={handleScrollLeft}
+              style={{
+                position: 'absolute',
+                left: 0,
+                top: '50%',
+                transform: 'translateY(-50%)',
+                background: 'transparent',
+                border: 'none',
+                zIndex: 1,
+              }}
+            >
+              &#8592; {/* Strzałka w lewo */}
+            </button>
+          )}
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            aria-label="basic tabs example"
+          >
+            <Tab label={t("Users")} {...a11yProps(0)} />
+            <Tab label={t("License")} {...a11yProps(1)} />
+            <Tab label={t("Settings")} {...a11yProps(2)} />
+            <Tab label={t("Services")} {...a11yProps(3)} />
+            <Tab label={t("Contractors")} {...a11yProps(4)} />
+            <Tab label={t("Task type")} {...a11yProps(5)} />
+          </Tabs>
+          {value < 5 && (
+            <button
+              onClick={handleScrollRight}
+              style={{
+                position: 'absolute',
+                right: 0,
+                top: '50%',
+                transform: 'translateY(-50%)',
+                background: 'transparent',
+                border: 'none',
+                zIndex: 1,
+              }}
+            >
+              &#8594; {/* Strzałka w prawo */}
+            </button>
+          )}
+        </Box>
+        <TabPanel value={value} index={0}>
+          <Users></Users>
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <License></License>
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          <Settings></Settings>
+        </TabPanel>
+        <TabPanel value={value} index={3}>
+          <ServicesDictionary></ServicesDictionary>
+        </TabPanel>
+        <TabPanel value={value} index={4}>
+          <Contractors></Contractors>
+        </TabPanel>
+        <TabPanel value={value} index={5}>
+          <TaskTypeDictionary></TaskTypeDictionary>
+        </TabPanel>
       </Box>
-      <TabPanel value={value} index={0}>
-     <Users></Users>
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-    <License></License>
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-    <Settings></Settings>
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-    <ServicesDictionary></ServicesDictionary>
-      </TabPanel>
-      <TabPanel value={value} index={4}>
-    <Contractors></Contractors>
-      </TabPanel>
-      <TabPanel value={value} index={5}>
-    <TaskTypeDictionary></TaskTypeDictionary>
-      </TabPanel>
-    </Box>
-      </div>   
-    );
-  }
-  
-  export default Administration;
+    </div>
+  );
+}
+
+export default Administration;
--- taskvenue/src/Pages/Manager/TaskDetails.js
+++ taskvenue/src/Pages/Manager/TaskDetails.js
@@ -14,72 +14,123 @@ import ManagerTaskBreadcrumbs from '../../Components/Breadcrumbs/ManagerTaskBrea
 
 
 function TabPanel(props) {
-    const { children, value, index, ...other } = props;
-  
-    return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box sx={{ p: 3 }}>
-            <Typography component={'span'} variant={'body2'}>{children}</Typography>
-          </Box>
-        )}
-      </div>
-    );
-  }
-  
-  TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.number.isRequired,
-    value: PropTypes.number.isRequired,
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography component={'span'} variant={'body2'}>
+            {children}
+          </Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
   };
-  
-  function a11yProps(index) {
-    return {
-      id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`,
-    };
-  }
+}
 
 function TaskDetails() {
-    const { t, i18n } = useTranslation();
-    const [value, setValue] = useState(0);
-    const handleChange = (event, newValue) => {
-      setValue(newValue);
-    };
-    return (
-      <div>
-             <ManagerNavBar></ManagerNavBar>
-             <ManagerTaskBreadcrumbs></ManagerTaskBreadcrumbs>
-            <Box sx={{ width: '100%' }}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-          <Tab label={t("Basic data")} {...a11yProps(0)} />
-          <Tab label={t("Venue")} {...a11yProps(1)} />
-          <Tab label={t("Services")} {...a11yProps(2)} />
-          <Tab label={t("Working time")} {...a11yProps(3)} />
-        </Tabs>
+  const { t, i18n } = useTranslation();
+  const [value, setValue] = useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleScrollRight = () => {
+    if (value < 3) {
+      setValue(value + 1);
+    }
+  };
+
+  const handleScrollLeft = () => {
+    if (value > 0) {
+      setValue(value - 1);
+    }
+  };
+
+  return (
+    <div>
+      <ManagerNavBar></ManagerNavBar>
+      <ManagerTaskBreadcrumbs></ManagerTaskBreadcrumbs>
+      <Box sx={{ width: '100%' }}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider', position: 'relative' }}>
+          {value > 0 && (
+            <button
+              onClick={handleScrollLeft}
+              style={{
+                position: 'absolute',
+                left: 0,
+                top: '50%',
+                transform: 'translateY(-50%)',
+                background: 'transparent',
+                border: 'none',
+                zIndex: 1,
+              }}
+            >
+              &#8592;
+            </button>
+          )}
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            aria-label="basic tabs example"
+          >
+            <Tab label={t("Basic data")} {...a11yProps(0)} />
+            <Tab label={t("Venue")} {...a11yProps(1)} />
+            <Tab label={t("Services")} {...a11yProps(2)} />
+            <Tab label={t("Working time")} {...a11yProps(3)} />
+          </Tabs>
+          {value < 3 && (
+            <button
+              onClick={handleScrollRight}
+              style={{
+                position: 'absolute',
+                right: 0,
+                top: '50%',
+                transform: 'translateY(-50%)',
+                background: 'transparent',
+                border: 'none',
+                zIndex: 1,
+              }}
+            >
+              &#8594; 
+            </button>
+          )}
+        </Box>
+        <TabPanel value={value} index={0}>
+          <ManagerBasicDataEdit></ManagerBasicDataEdit>
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <ManagerVenueEdit></ManagerVenueEdit>
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          <ManagerServicesEdit></ManagerServicesEdit>
+        </TabPanel>
+        <TabPanel value={value} index={3}>
+          <ManagerWorkingTimeEdit></ManagerWorkingTimeEdit>
+        </TabPanel>
       </Box>
-      <TabPanel value={value} index={0}>
-        <ManagerBasicDataEdit></ManagerBasicDataEdit>
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-     <ManagerVenueEdit></ManagerVenueEdit>
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-     <ManagerServicesEdit></ManagerServicesEdit>
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-    <ManagerWorkingTimeEdit></ManagerWorkingTimeEdit>
-      </TabPanel>
-    </Box>
-      </div>   
-    );
-  }
-  
-  export default TaskDetails;
+    </div>
+  );
+}
+
+export default TaskDetails;
