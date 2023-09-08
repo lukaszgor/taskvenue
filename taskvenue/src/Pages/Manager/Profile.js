17f2b051b217cfa8e634bb3831c16a689143517c
Lukaszgor <lukasz.gg13@gmail.com>
2023-09-08 21:27:05 +0200

    added breadcrumbs to profile

--- taskvenue/src/Pages/Manager/Profile.js
+++ taskvenue/src/Pages/Manager/Profile.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ManagerNavBar from '../../Components/NavigationBar/ManagerNavBar';
 import PasswordChange from '../../Components/Common/PasswordChange';
+import ManagerProfileBreadcrumbs from '../../Components/Breadcrumbs/mainBreadcrumbs/ManagerProfileBreadcrumbs';
 
 const Profile = () => {
     return (
         <div>
             <ManagerNavBar></ManagerNavBar>
+            <ManagerProfileBreadcrumbs></ManagerProfileBreadcrumbs>
          <PasswordChange></PasswordChange>
 
       
