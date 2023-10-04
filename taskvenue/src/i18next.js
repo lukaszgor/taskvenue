9e762575b6414d5416ee93bfae12a2b0afe0853b
Lukaszgor <lukasz.gg13@gmail.com>
2023-10-04 16:24:17 +0200

    default language PL

--- taskvenue/src/i18next.js
+++ taskvenue/src/i18next.js
@@ -18,7 +18,7 @@ i18next.use(initReactI18next).init({
 			translation: de,
 		},
 	},
-	lng: localStorage.getItem("lng") || "en",
+	lng: localStorage.getItem("lng") || "pl",
 });
 
 export default i18next;
