b3fde5a9e5eed796cfd59d82d72badfd8c7c753c
Lukaszgor <lukasz.gg13@gmail.com>
2023-10-20 12:12:33 +0200

    added some changes in headerInfo

--- taskvenue/src/Components/Info/MainPage/HeaderInfo.js
+++ taskvenue/src/Components/Info/MainPage/HeaderInfo.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Container, Grid, Typography } from '@mui/material';
+import { Button, Card, CardContent, Container, Grid, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-
+import { Link } from 'react-router-dom'; 
 
 const HeaderInfo = () => {
   const navigate = useNavigate();
@@ -14,7 +14,7 @@ const HeaderInfo = () => {
       <Container maxWidth="lg">
         <Grid container spacing={3}>
           <Grid item xs={12} md={6}>
-            <Typography variant="h3" align="left" 
+            <Typography variant="h2" align="left" 
                       sx={{
                         mr: 2,
                         mt: 10,
@@ -26,9 +26,21 @@ const HeaderInfo = () => {
                     >
               TaskVenue
             </Typography>
-            <Typography variant="h5" align="left" sx={{ mt: 2 }}>
+            <Typography variant="h4" align="left" sx={{ mt: 2 }}>
               {t("A place for your business")}
             </Typography>
+            <p></p>
+
+                  <Typography variant="h6" align='center' sx={{ mt: 2, marginTop: 10, marginBottom: 2 }}>
+                    {t("That is, a system that improves management and communication between management, the customer, and those doing the work in the field.")}
+                  </Typography>
+                  <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <Link to="/infocontact">
+                      <Button variant="contained" color="primary">
+                        {t("Order a consultation")}
+                      </Button>
+                    </Link>
+                  </div>
           </Grid>
           <Grid item xs={12} md={6}>
             <img
