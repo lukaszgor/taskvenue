a64a25b9d335361420f991d63908c699cdf3db11
Lukaszgor <lukasz.gg13@gmail.com>
2023-10-20 16:42:25 +0200

    changes in TargetCompany file

--- taskvenue/src/Components/Info/MainPage/TargetCompany.js
+++ taskvenue/src/Components/Info/MainPage/TargetCompany.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { Divider } from '@mui/material';
-
+import { Accordion, AccordionSummary, AccordionDetails, Typography, Box, Container, Grid } from '@mui/material';
 import { Link } from 'react-router-dom'; 
 import { useTranslation } from 'react-i18next';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 const centerStyles = {
   display: 'flex',
   flexDirection: 'column',
@@ -27,49 +27,71 @@ const TargetCompany = () => {
       <Typography variant="h4" gutterBottom style={contentStyles}>
           Dla kogo?
         </Typography>
+        <Accordion >
+  <AccordionSummary expandIcon={<ExpandMoreIcon />}>
 <Divider>Branża usługowa</Divider>
-<p></p>
+</AccordionSummary>
+<AccordionDetails>
 Firmy oferujące różnego rodzaju usługi, takie jak sprzątanie, konserwacja, naprawy itp., mogą korzystać z tej aplikacji do lepszej organizacji swojej pracy i monitorowania wyników.
-<p></p>
+</AccordionDetails>
+</Accordion>
+
+<Accordion >
+  <AccordionSummary expandIcon={<ExpandMoreIcon />}>
 <Divider>Obsługa nieruchomości</Divider>
-<p></p>
+</AccordionSummary>
+<AccordionDetails>
 Aplikacja ta może być wykorzystywana przez firmy zarządzające nieruchomościami do monitorowania i zarządzania zadaniami związanymi z utrzymaniem budynków, naprawami, przeglądami, czyszczeniem itp.
-<p></p>
+</AccordionDetails>
+</Accordion>
+
+<Accordion >
+  <AccordionSummary expandIcon={<ExpandMoreIcon />}>
 <Divider>Serwis techniczny</Divider>
-<p></p>
+</AccordionSummary>
+<AccordionDetails>
 Firmy świadczące usługi serwisowe dla różnych urządzeń i maszyn mogą korzystać z tej aplikacji do rejestrowania lokalizacji urządzeń, przypisywania zadań serwisowych i śledzenia postępów prac.
-<p></p>
+</AccordionDetails>
+</Accordion>
+
+<Accordion >
+  <AccordionSummary expandIcon={<ExpandMoreIcon />}>
 <Divider>Zarządzanie projektami budowlanymi</Divider>
-<p></p>
+</AccordionSummary>
+<AccordionDetails>
 W branży budowlanej aplikacja ta może pomóc w rejestrowaniu lokalizacji różnych etapów projektu, przypisywaniu zadań podwykonawcom i monitorowaniu postępów prac na budowie.
-<p></p>
-<Divider>Usługi dostaw i logistyka</Divider>
-<p></p>
-Firmy zajmujące się dostawami i logistyką mogą wykorzystywać tę aplikację do śledzenia lokalizacji pojazdów, przypisywania zadań kierowcom i monitorowania dostaw.
-<p></p>
-<Divider>Przemysł produkcyjny</Divider>
-<p></p>
-W fabrykach i zakładach produkcyjnych aplikacja ta może pomóc w zarządzaniu utrzymaniem ruchu, konserwacją maszyn i urządzeń, a także monitorowaniu ich wydajności.
-<p></p>
-<Divider>Przemysł produkcyjny</Divider>
-<p></p>
-W fabrykach i zakładach produkcyjnych aplikacja ta może pomóc w zarządzaniu utrzymaniem ruchu, konserwacją maszyn i urządzeń, a także monitorowaniu ich wydajności.
-<p></p>
-<Divider>Zarządzanie budynkami i obiektami użyteczności publicznej</Divider>
-<p></p>
+</AccordionDetails>
+</Accordion>
+
+<Accordion >
+  <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+<Divider>Zarządzanie nieruchomościami</Divider>
+</AccordionSummary>
+<AccordionDetails>
 W przypadku obiektów użyteczności publicznej, takich jak szkoły, szpitale lub instytucje rządowe, aplikacja ta może pomóc w zarządzaniu utrzymaniem, czyszczeniem i innymi zadaniami.
-<p></p>
-<Divider>Zarządzanie nieruchomościami komercyjnymi</Divider>
-<p></p>
 W przypadku zarządzania biurami, centrów handlowych lub innymi obiektami komercyjnymi, aplikacja ta może pomóc w monitorowaniu i zarządzaniu różnymi aspektami obsługi i utrzymania nieruchomości.
-<p></p>
+</AccordionDetails>
+</Accordion>
+
+<Accordion >
+  <AccordionSummary expandIcon={<ExpandMoreIcon />}>
 <Divider>Rolnictwo</Divider>
-<p></p>
+</AccordionSummary>
+<AccordionDetails>
 W rolnictwie aplikacja ta może być wykorzystywana do zarządzania pracami na farmach, planowania i monitorowania zadań związanych z uprawą, nawadnianiem i pielęgnacją roślin.
-<p></p>
+</AccordionDetails>
+</Accordion>
+
+
+<Accordion >
+  <AccordionSummary expandIcon={<ExpandMoreIcon />}>
 <Divider>Zarządzanie projektami</Divider>
-<p></p>
+</AccordionSummary>
+<AccordionDetails>
 Niezależnie od branży, aplikacja ta może być używana do zarządzania projektami, w których istnieje potrzeba monitorowania lokalizacji, zadań i postępów prac.
+</AccordionDetails>
+</Accordion>
+<p></p>
       </Container>
     </div>
   );
