2a56c30daeac819adc93ca61b41ab50049ea9583
Lukaszgor <lukasz.gg13@gmail.com>
2023-12-18 17:42:07 +0100

    servicesDictionary improved

--- taskvenue/src/Components/Manager/Administration/ServicesDictionary.js
+++ taskvenue/src/Components/Manager/Administration/ServicesDictionary.js
@@ -1,14 +1,27 @@
-
-import { useState,useEffect } from 'react';
-import { DataGrid } from '@mui/x-data-grid';
-import { TextField, Button, Grid, Container, Typography,Box,Accordion, AccordionSummary, AccordionDetails  } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import {
+  TextField,
+  Button,
+  Grid,
+  Container,
+  Typography,
+  Box,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Card,
+  CardContent,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import supabase from '../../../supabaseClient';
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
-import { useTranslation } from "react-i18next";
-import Tooltip from '@mui/material/Tooltip';
+import { useTranslation } from 'react-i18next';
 
 function ServicesDictionary() {
   const { t, i18n } = useTranslation();
@@ -16,261 +29,335 @@ function ServicesDictionary() {
   const [description, setDescription] = useState('');
   const [cost, setCost] = useState(0);
   const [unit, setUnit] = useState('');
-  const [fetchError,setFetchError] =useState(null)
-  const [service,setService] =useState(null)
-  const {id} = useParams()
+  const [fetchError, setFetchError] = useState(null);
+  const [service, setService] = useState(null);
+
+  // Dialog state
+  const [openEditDialog, setOpenEditDialog] = useState(false);
+  const [selectedService, setSelectedService] = useState(null);
+
 
   const [userID, setUserID] = useState('');
   const [idConfig, setIdConfiguration] = useState('');
-  
-      useEffect(() => {
-          const checkSession = async () => {
-            const { data } = await supabase.auth.getSession();
-            if (data.session) {
-              setUserID(data.session.user.id);
-              fetchData(data.session.user.id);
-            }
-          };
-          checkSession();
-        }, []);
-        
-        const fetchData = async (userId) => {
-          const { data: profileData, error: profileError } = await supabase
-              .from('profiles')
-              .select('id_configuration')
-              .eq('id', userId)
-              .single();
-          if (profileError) {
-              console.log(profileError);
-          } else if (profileData) {
-              setIdConfiguration(profileData.id_configuration);
-          }
+
+  useEffect(() => {
+    const checkSession = async () => {
+      const { data } = await supabase.auth.getSession();
+      if (data.session) {
+        setUserID(data.session.user.id);
+        fetchData(data.session.user.id);
       }
-   useEffect(() => {
-          if (idConfig) {
-            fetchServices(idConfig)
-            
-          }
-        }, [idConfig]);
-  
+    };
+    checkSession();
+  }, []);
+
+  const fetchData = async (userId) => {
+    const { data: profileData, error: profileError } = await supabase
+      .from('profiles')
+      .select('id_configuration')
+      .eq('id', userId)
+      .single();
+    if (profileError) {
+      console.log(profileError);
+    } else if (profileData) {
+      setIdConfiguration(profileData.id_configuration);
+    }
+  };
+
+  useEffect(() => {
+    if (idConfig) {
+      fetchServices(idConfig);
+    }
+  }, [idConfig]);
+
   const handleSubmit = (event) => {
     event.preventDefault();
     insertService();
     setName('');
     setDescription('');
-    setCost('');
+    setCost(0);
     setUnit('');
   };
 
-  //Delete
-  const DeleteSerice = async(event, cellValues)=>{
-      const{data,error} =  await supabase
-  .from('service_dictionary')
-  .delete().eq('id', cellValues.row.id);
-  handleClickAlert();
-  fetchServices(idConfig);
-  if(error){
-      console.log(error)
-  }if(data){
-   
-  }
-  }
-    //insert
-const insertService = async()=>{
-  const{data,error} =  await supabase
-  .from('service_dictionary')
-  .insert([{id_configuration:idConfig,name:name,cost:cost,description:description,unit:unit}])
-  handleClickAlert()
-  fetchServices(idConfig)
-  if(error){
-      console.log(error)
-  }if(data){
-   
-  }
-}
+  //insert
+  const insertService = async () => {
+    const { data, error } = await supabase
+      .from('service_dictionary')
+      .insert([
+        {
+          id_configuration: idConfig,
+          name: name,
+          cost: cost,
+          description: description,
+          unit: unit,
+        },
+      ]);
+    handleClickAlert();
+    fetchServices(idConfig);
+    if (error) {
+      console.log(error);
+    }
+    if (data) {
+    }
+  };
+  const handleDeleteClick = async (service) => {
+    await supabase
+      .from('service_dictionary')
+      .delete()
+      .eq('id', service.id);
 
-//download data
-    const fetchServices = async(idConfiguration)=>{
-      const{data,error} =  await supabase
+      fetchServices(idConfig);
+
+    handleClickAlert();
+  };
+  //download data
+  const fetchServices = async (idConfig) => {
+    const { data, error } = await supabase
       .from('service_dictionary')
       .select()
-      .eq('id_configuration', idConfiguration);
-      if(error){
-          console.log(error)
-          setService(null)
-          setFetchError(t("No services"))
-      }if(data){
-        setService(data)
-        setFetchError(null)
-      }
-  }
+      .eq('id_configuration', idConfig)
+    if (error) {
+      console.log(error);
+      setService(null);
+      setFetchError(t('No products'));
+    }
+    if (data) {
+      setService(data);
+      setFetchError(null);
+    }
+  };
 
   //alert configuration
-  const [open,setOpen] =useState(null)
+  const [open, setOpen] = useState(null);
 
   const handleClickAlert = () => {
     setOpen(true);
   };
-  
+
   const handleCloseAlert = (event, reason) => {
     if (reason === 'clickaway') {
       return;
     }
     setOpen(false);
   };
-  
 
-  const columns = [
-      {
-        field: 'name',
-        headerName: t("Name"),
-        width: 300, 
-        renderCell: (params) => (
-          <Tooltip title={params.value} arrow>
-            <div style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '100%' }}>
-              {params.value}
-            </div>
-          </Tooltip>
-        ),
-      },
-      {
-        field: 'description',
-        headerName: t("Description"),
-        width: 600, 
-        renderCell: (params) => (
-          <Tooltip title={params.value} arrow>
-            <div style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '100%' }}>
-              {params.value}
-            </div>
-          </Tooltip>
-        ),
-      },
-      { field: 'cost', headerName: t("Cost"), width: 80 },
-      { field: 'unit', headerName: t("Unit"), width: 220 },
-      {
-          field: "Action",headerName: t("Action"), width: 200 ,
-          renderCell: (cellValues) => {
-            return ( 
-              <Button
-              color="error"
-              onClick={(event) => {
-                  DeleteSerice(event, cellValues);
-              }}
-              >{t("Delete")}</Button>
-            );
-          }
-        },
-     
-    ];
-    return (
+  // Dialog functions
+  const handleEditClick = (service) => {
+    setSelectedService(service);
+    setName(service.name);
+    setDescription(service.description);
+    setCost(service.cost);
+    setUnit(service.unit);
+    setOpenEditDialog(true);
+  };
+
+  const handleCloseEditDialog = () => {
+    setOpenEditDialog(false);
+  };
+
+  const handleSaveEdit = async () => {
+    await supabase
+      .from('service_dictionary')
+      .update({
+        name,
+        cost,
+        description,
+        unit,
+      })
+      .eq('id', selectedService.id);
+
+      fetchServices(idConfig);
+
+    setOpenEditDialog(false);
+
+    handleClickAlert();
+  };
+
+  return (
+    <div>
       <div>
-            <div>
-              <p></p>
-              <Container maxWidth="md">
-              <Accordion >
-        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography variant="h6" fontWeight="bold">{t('Add')}</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography></Typography>
-          <form onSubmit={handleSubmit} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <p></p>
+        <Container maxWidth="md">
+          <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography variant="h6" fontWeight="bold">
+                {t('Add')}
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography></Typography>
+              <form
+                onSubmit={handleSubmit}
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <Container maxWidth="md">
+                  <Typography variant="h4" align="center" gutterBottom>
+                    <p></p>
+                  </Typography>
+                  <Grid container spacing={2}>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        label={t('Name')}
+                        value={name}
+                        onChange={(event) => setName(event.target.value)}
+                        style={{ marginRight: '10px' }}
+                        fullWidth
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        label={t('Description')}
+                        value={description}
+                        onChange={(event) =>
+                          setDescription(event.target.value)
+                        }
+                        style={{ marginRight: '10px' }}
+                        fullWidth
+                        multiline
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        label={t('Cost')}
+                        value={cost}
+                        onChange={(event) => setCost(event.target.value)}
+                        style={{ marginRight: '10px' }}
+                        type="number"
+                        fullWidth
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        label={t('Unit')}
+                        value={unit}
+                        onChange={(event) => setUnit(event.target.value)}
+                        style={{ marginRight: '10px' }}
+                        fullWidth
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Box display="flex" justifyContent="flex-end">
+                        <Button
+                          type="submit"
+                          variant="contained"
+                          color="primary"
+                          style={{ minWidth: 'auto' }}
+                        >
+                          {t('Submit')}
+                        </Button>
+                      </Box>
+                    </Grid>
+                  </Grid>
+                  <div></div>
+                  <Snackbar
+                    open={open}
+                    autoHideDuration={2000}
+                    onClose={handleCloseAlert}
+                  >
+                    <Alert severity="success"> {t('Updated!')}</Alert>
+                  </Snackbar>
+                </Container>
+              </form>
+            </AccordionDetails>
+          </Accordion>
 
-<Container maxWidth="md">
- <Typography variant="h4" align="center" gutterBottom>
-<p></p>
- </Typography>
-   <Grid container spacing={2}>
-     <Grid item xs={12} sm={6}>
-     <TextField
-             label={t("Name")}
-             value={name}
-             onChange={(event) => setName(event.target.value)}
-             style={{ marginRight: '10px' }}
-             fullWidth
-         />
-     </Grid>
-     <Grid item xs={12} sm={6}>
-     <TextField
-             label={t("Description")}
-             value={description}
-             onChange={(event) => setDescription(event.target.value)}
-             style={{ marginRight: '10px' }}
-             fullWidth
-             multiline
-         />
-     </Grid>
-     <Grid item xs={12} sm={6}>
-     <TextField
-             label={t("Cost")}
-             value={cost}
-             onChange={(event) => setCost(event.target.value)}
-             style={{ marginRight: '10px' }}
-             type="number" 
-             fullWidth
-         />
-     </Grid>
-     <Grid item xs={12} sm={6}>
-     <TextField
-             label={t("Unit")}
-             value={unit}
-             onChange={(event) => setUnit(event.target.value)}
-             style={{ marginRight: '10px' }}
-             fullWidth
-         />
-     </Grid>
-     <Grid item xs={12}>
-       <Box display="flex" justifyContent="flex-end">
-         <Button
-         type="submit"
-         variant="contained"
-         color="primary"
-         style={{ minWidth: 'auto' }}
-         >
-         {t('Submit')}
-         </Button>
-     </Box>
-     </Grid>
-   </Grid>
-   <div>
-</div>
- <Snackbar open={open}
-     autoHideDuration={2000}
-     onClose={handleCloseAlert}>
-   <Alert severity="success"> {t("Updated!")}!</Alert>
-   </Snackbar>
-</Container>
-</form>
-        </AccordionDetails>
-      </Accordion>
-      </Container>
- <div>
-     {fetchError &&(<p>{fetchError}</p>)}
-     {service &&(
-     <div>
-   <p> </p>
-   <div style={{ height: 400, width: '100%' }}>
-         <DataGrid
-           rows={service}
-           columns={columns}
-           pageSize={12}
-           rowsPerPageOptions={[12]}
-         />
-       </div>
-   <div>
-   </div>
-     </div>
-     )}
-   </div>
-   {/* <Snackbar open={open}
-     autoHideDuration={2000}
-     onClose={handleCloseAlert}>
-   <Alert severity="success">{t("Updated!")}</Alert>
-   </Snackbar> */}
-   </div>
-   
+          <div>
+            <p></p>
+            <Grid container spacing={3}>
+              {service &&
+                service.sort((a, b) => b.id - a.id).map((item) => {
+                  return (
+                    <Grid item xs={12} sm={6} md={6} lg={4} key={item.id}>
+                      <Card>
+                        <CardContent>
+                          <Typography variant="h6">{item.name}</Typography>
+                          <Typography>{item.description}</Typography>
+                          <Typography>
+                            {t('Cost')}: {item.cost}
+                          </Typography>
+                          <Typography>
+                            {t('Unit')}: {item.unit}
+                          </Typography>
+                          <Box display="inline-block" marginRight={1}>
+                          <Button
+                            variant="outlined"
+                            color="primary"
+                            onClick={() => handleEditClick(item)}
+                          >
+                            {t('Edit')}
+                          </Button>
+                          </Box>
+                          <Box display="inline-block" marginRight={1}>
+                          <Button
+                            variant="outlined"
+                            color="secondary"
+                            onClick={() => handleDeleteClick(item)}
+                          >
+                            {t('Delete')}
+                          </Button>
+                          </Box>
+                        </CardContent>
+                      </Card>
+                    </Grid>
+                  );
+                })}
+            </Grid>
+          </div>
+        </Container>
       </div>
-    );
-  }
-  
-  export default ServicesDictionary;
-  
+
+      {/* Edit Dialog */}
+      <Dialog open={openEditDialog} onClose={handleCloseEditDialog}>
+        <DialogTitle>{t('Edit')}</DialogTitle>
+        <DialogContent>
+        <p></p>
+          <TextField
+            label={t('Name')}
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+            style={{ marginBottom: '10px' }}
+            fullWidth
+          />
+          <TextField
+            label={t('Description')}
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+            style={{ marginBottom: '10px' }}
+            fullWidth
+            multiline
+          />
+          <TextField
+            label={t('Cost')}
+            value={cost}
+            onChange={(event) => setCost(event.target.value)}
+            style={{ marginBottom: '10px' }}
+            type="number"
+            fullWidth
+          />
+          <TextField
+            label={t('Unit')}
+            value={unit}
+            onChange={(event) => setUnit(event.target.value)}
+            style={{ marginBottom: '10px' }}
+            fullWidth
+          />
+
+        </DialogContent>
+        <DialogActions >
+          <Button onClick={handleCloseEditDialog} color="primary">
+            {t('Cancel')}
+          </Button>
+          <Button onClick={handleSaveEdit} color="primary">
+            {t('Save')}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
+
+export default ServicesDictionary;
